90021a7e6b0daf4ffed54c7cd463e17185d75285
Daniel Vagg <daniel.vagg@parameterspace.ie>
2016-10-23 22:26:43 +0100

    Fixing relative links

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ def readme():
         return f.read()
 
 setup(name='educube',
-      version='0.1',
-      download_url='https://github.com/ezeakeal/educube_client/tarball/0.1',
+      version='0.2',
+      download_url='https://github.com/ezeakeal/educube_client/tarball/0.2',
       keywords=['educube'],
       description='EduCube Client',
       long_description=readme(),
