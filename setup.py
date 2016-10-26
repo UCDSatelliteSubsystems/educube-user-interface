7e57d82e7a349ca659482b77d7819126673b74b6
Daniel Vagg <daniel.vagg@parameterspace.ie>
2016-10-27 00:09:05 +0100

    Getting ready for 0.3 release

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ def readme():
         return f.read()
 
 setup(name='educube',
-      version='0.2',
-      download_url='https://github.com/ezeakeal/educube_client/tarball/0.2',
+      version='0.3',
+      download_url='https://github.com/ezeakeal/educube_client/tarball/0.3',
       keywords=['educube'],
       description='EduCube Client',
       long_description=readme(),
