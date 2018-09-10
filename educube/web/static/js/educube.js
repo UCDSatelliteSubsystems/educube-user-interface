58b24c80719b928f8bd7c164f8e914ccf5c56d5c
RJeffrey <robjeffrey25@gmail.com>
2018-09-10 09:54:46 +0100

    Attempt to fix map resize bug -- failed

--- educube/web/static/js/educube.js
+++ educube/web/static/js/educube.js
@@ -13,8 +13,12 @@ function EduCubeClientSocket(port) {
         // this smells... I don't like that these components are so
         // interdependent that they have to be set up in a particular order!
 
+	// there is a bug here in how OpenLayers and Bootstrap work together,
+	// which means that the map doesn't appear until the window is
+	// resized. 
         console.log('Creating GPSMap');
         gps_map = new GPSMap('gps-map');
+	//        gps_map.updateSize();
         console.log('Creating GPSMap -- DONE');
 
         telemetryhandler = new TelemetryHandler(gps_map);
