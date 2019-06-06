aa4d5bc0eec883adfb48113a443237a74ac51cc3
RJeffrey <robjeffrey25@gmail.com>
2019-06-06 12:36:46 +0100

    Changed telemetry_parser to use abc.Mapping

--- educube/telemetry_parser/_util.py
+++ educube/telemetry_parser/_util.py
@@ -1,9 +1,9 @@
-from collections import Iterable
+from collections import Iterable, Mapping
 
 # function to allow us to convert namedtuple data hierarchy into JSON-able
 # form (i.e., into tuples, dictionaries, primitives)
 def serialise(obj):
-    """
+    """\
     Convert a hierarchy of namedtuples into primitives and dicts.
 
     This function recursively traverses a hierarchy of iterables, looking for
@@ -26,10 +26,13 @@ def serialise(obj):
         return obj
 
 def remove_value_none(d):
-    """Recursively traverse a dictionary to remove keys with value None."""
+    """\
+    Recursively traverse Mapping (eg. dict) to remove keys with value None.
+    """
     _dict = dict()
-    for key, val in d.items():
-        if isinstance(val, dict):
+    for key in d:
+        val = d[key]
+        if isinstance(val, Mapping):
             _dict[key] = remove_value_none(val)
         elif val is not None:
             _dict[key] = val
