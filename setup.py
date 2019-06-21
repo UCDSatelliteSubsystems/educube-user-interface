5c85290e69ff263d0ff407c7920d10229f13fa17
RJeffrey <robjeffrey25@gmail.com>
2019-06-21 18:25:02 +0100

    Added __version__.py, simpler setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,41 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.6.2dev'
+
+NAME             = 'educube'
+DESCRIPTION      = 'EduCube Client and User Interface'
+REQUIRES_PYTHON  = '>=3.4'
+KEYWORDS         = ('educube', )
+LICENSE          = 'GPLv3'
+INSTALL_REQUIRES = ('click', 'pyserial', 'tornado')
+
+ENTRY_POINTS = {'console_scripts': ('educube = educube.__main__:main', )}
+
+
+def version():
+    _namespace = {}
+    with open(os.path.join()) as f:
+        exec(f.read(), _namespace)
+
+    return _namespace['__version__']
+
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='educube',
-      version=VERSION,
-      download_url='https://github.com/ezeakeal/educube_client/tarball/0.5.9',
-      keywords=['educube'],
-      description='EduCube Client',
-      long_description=readme(),
-      author='UCD',
-      author_email='daniel.vagg@gmail.com',
-      license='GPLv3',
-      packages=find_packages(),
-      include_package_data = True,
-      install_requires=[
-        'click',
-        'pyserial',
-        'tornado',
-      ],
-      tests_require=[
-        'nose',
-      ],
-      python_requires=">=3.4",
-      entry_points={
-        'console_scripts': [
-          'educube = educube.__main__:main',
-        ],
-      },
-      dependency_links=[
-      ],
-      test_suite='nose.collector',
-      zip_safe=False)
+
+
+setup(
+    name                 = NAME,
+    version              = version(),
+    keywords             = KEYWORDS,
+    description          = DESCRIPTION,
+    long_description     = readme(),
+    license              = LICENSE,
+    packages             = find_packages(),
+    include_package_data = True,
+    install_requires     = INSTALL_REQUIRES,
+    python_requires      = REQUIRES_PYTHON,
+    entry_points         = ENTRY_POINTS,
+    zip_safe             = False
+)
